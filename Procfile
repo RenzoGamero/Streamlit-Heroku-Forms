e271cec8c45f0ad0ae0e911fb4eb3c7f977722ae
Renzo Gamero R <renzogameror@MacBook-Pro-de-Renzo.local>
2022-01-04 11:12:51 -0500

    S_H_test.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run S_H_test.py
---
+++ S_H_test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+
+st.write('Test Form Ipress')
+
+st.write('Pregunta 1')
+st.write('Pregunta 2')
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
