4738ea716a94866e2702d794f5c4cedd0fe2cbed
RenzoGamero <renzogamero@gmail.com>
2022-01-24 16:58:04 -0500

    202201241658

--- app.py
+++ app.py
@@ -704,11 +704,11 @@ def expanderrr(x, q, op, tipo, qe, nivel,vista, DependenciaSiNo, Validar):
                    }  # etc.
 
 
-            st.write('PreguntaObj PreguntaObj   : ', df[(df['q_'] == PreguntaObj)]['resp'].values[0])
+            st.write('PreguntaObj PreguntaObj   : ', df[(df['q_'] == int(PreguntaObj))]['resp'].values[0])
             st.write('PreguntaObj qe            : ', df[(df['q_'] == qe)]['resp'].values[0])
 
 
-            st.write('result3: ', ops[StringOperator](df[(df['q_'] == PreguntaObj)]['resp'].values[0], df[(df['q_'] == qe)]['resp'].values[0]))
+            st.write('result3: ', ops[StringOperator](df[(df['q_'] == int(PreguntaObj)]['resp'].values[0], df[(df['q_'] == qe)]['resp'].values[0]))
 
         #print(ops["+"](1, 1))  # prints 2
         #st.write('result3: ', ops["+"](1, 1))
