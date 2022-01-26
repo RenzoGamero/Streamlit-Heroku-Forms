ffa0d3a37c392d0ab8258f445492f292a70fd84c
RenzoGamero <renzogamero@gmail.com>
2022-01-26 12:19:16 -0500

    202201261219

--- app.py
+++ app.py
@@ -1426,8 +1426,8 @@ for i in result:
         dff1 = sheetDataCheck[(sheetDataCheck['Sector'] == str(option))]
         st.write('dff1= ', dff1)
 
-        st.write('Nombre_Indicador 1= ', dff1['Nombre_Indicador'][0].values[0])
-        st.write('Nombre_Indicador 2= ', dff1['Nombre_Indicador'][1].values[0])
+        st.write('Nombre_Indicador 1= ', dff1['Nombre_Indicador'][0])
+        st.write('Nombre_Indicador 2= ', dff1['Nombre_Indicador'][1])
         # st.dataframe(data=dff1, width=None, height=None)
 
         # formula = "var5/var6"
